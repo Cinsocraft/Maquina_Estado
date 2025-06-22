845fe3292556644361d8fc88b9c9261d532b063e
Cinsocraft <cinoscraft@gmail.com>
2025-06-22 12:56:58 +0200

    subida

--- Assets/fall_state.gd
+++ Assets/fall_state.gd
@@ -11,5 +11,11 @@ func on_physics_process(delta):
 	if Input.get_axis("left", "right") and player.is_on_floor(): 
 		state_machine.change_to(player.states.Move)
 	
+	
+	
+	#coyote_timer
+	#coyote_time_actived
+	
 	handle_gravity(delta)
 	controlled_node.move_and_slide()
+	
--- Assets/jump_state.gd
+++ Assets/jump_state.gd
@@ -2,31 +2,13 @@ extends PlayerStateGravityBase
 
 func 	on_physics_process(delta):
 	player.velocity.x = \
-		Input.get_axis("left", "right") * player.movement_stats.move_speed
-		
+	Input.get_axis("left", "right") * player.movement_stats.move_speed
+	
 	# si esta en el suelo y esta parado sobre él, podemos darle impulso de salto
 	if controlled_node.is_on_floor() and controlled_node.velocity.y >= 0: 
 		controlled_node.velocity.y = controlled_node.movement_stats.jump_speed
-		
-	# en otro caso, si está bajando, cambiamos al estado de cayendo
 	elif controlled_node.velocity.y > 0: state_machine.change_to(player.states.Fall)
-	###################
-	
-	
-	if controlled_node.get_last_slide_collision() and controlled_node.velocity.y >= 0: 
-		controlled_node.velocity.y = controlled_node.movement_stats.jump_speed
-		
-	if controlled_node.is_on_floor():
-		controlled_node.movement_stats.coyote_time_actived=false
-	else:
-		if controlled_node.coyote_timer.is_stopped():
-			controlled_node.coyote_timer.start()
-			controlled_node.movement_stats.coyote_time_actived=true
-		
-		
-	
-	#coyote_timer
-	#coyote_time_actived
 	
 	handle_gravity(delta)
 	controlled_node.move_and_slide()
+	
